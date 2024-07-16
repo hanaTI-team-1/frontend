10b06b716bfc9ac9993f2d1208ae5621ebc56543
mrbonk97 <mrbonk97@gmail.com>
2024-07-16 15:16:29 +0900

    매물 리스트 수정

--- src/components/card/JeonseCard2.jsx
+++ src/components/card/JeonseCard2.jsx
@@ -1,56 +1,93 @@
 import { IoIosArrowDroprightCircle } from "react-icons/io";
 import { MdApartment } from "react-icons/md";
 import {
+  Button,
   Drawer,
   DrawerBody,
+  DrawerCloseButton,
   DrawerContent,
+  DrawerFooter,
   DrawerHeader,
   DrawerOverlay,
   useDisclosure,
 } from "@chakra-ui/react";
-import React, { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import KakaoMap from "../KakaoMap";
+import { useRef } from "react";
 
-export const JeonseCard2 = ({ info }) => {
+export const JeonseCard2 = ({ info, url }) => {
   console.log(info);
-  const { id } = useParams();
-  const navigate = useNavigate();
+  console.log(info.address);
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef = React.useRef();
+  const btnRef = useRef();
 
   return (
     <li
-      className="w-full h-36 py-5 px-10 flex gap-10 rounded-xl shadow-lg bg-slate-50 border"
+      className="w-full h-32 py-5 px-10 flex justify-between rounded-xl shadow-lg bg-slate-50 border hover:bg-slate-100 duration-200 cursor-pointer"
       ref={btnRef}
       onClick={onOpen}
     >
-      <div className="h-full w-24 flex items-center justify-center bg-rose-300">
-        <div className="h-16 w-16 flex items-center justify-center rounded-lg bg-gray-100">
-          <MdApartment className="text-blue-400" size={48} />
+      <div className="flex gap-10">
+        <div className="h-full w-24 flex items-center justify-center">
+          <div className="h-16 w-16 flex items-center justify-center rounded-lg bg-gray-200">
+            <MdApartment className="text-blue-400" size={48} />
+          </div>
         </div>
+        <hgroup className="flex flex-col gap-1">
+          <span className="text-2xl font-bold">
+            {info.atclNm} {info.bildNm}
+          </span>
+          <span className="font-medium text-lg">{info.hanPrc}원</span>
+          <span className="opacity-70">{info.spc2 + "평"}</span>
+        </hgroup>
       </div>
-      <hgroup className="flex flex-col">
-        <span className="text-2xl font-bold">{info.atclNm}</span>
-        <span className="font-medium text-lg">{info.hanPrc}원</span>
-        <span className="opacity-70">{info.spc2 + "평"}</span>
-      </hgroup>
-      {/* <div className="flex justify-between items-center">
-        <div className="flex items-center">
-          <div className="h-20 w-20 flex items-center justify-center bg-slate-200 rounded-lg">
-            <MdApartment size={48} />
-          </div>
-          <div className="ml-5 flex flex-col">
-            <span className="font-bold text-lg">{protect.address}</span>
-            <span className="text-xs opacity-40">
-              {protect.atclNm + " " + protect.spc2 + "평"}
-            </span>
-            <span className="font-bold text-lg">{protect.hanPrc}원</span>
-          </div>
-        </div>
-        <div className="mr-5">
-          <IoIosArrowDroprightCircle size={48} className="text-gray-500" />
-        </div>
-      </div> */}
+      <div className="h-full flex items-center">
+        <IoIosArrowDroprightCircle size={48} className="text-gray-500" />
+      </div>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size={"md"}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>전세 상세 정보</DrawerHeader>
+
+          <DrawerBody>
+            <KakaoMap lat={info.lat} lng={info.lng} atclNm={info.atclNm} />
+            <div>
+              <span className="text-lg">{info.address}</span>
+              <ul className="mt-5 space-y-1">
+                <li className="grid grid-cols-10 gap-2">
+                  <span className="col-span-2 text-right">특징</span>
+                  <span className="col-span-8">{info.atclFetrDesc}</span>
+                </li>
+                <li className="grid grid-cols-10 gap-2">
+                  <span className="col-span-2 text-right">방향</span>
+                  <span className="col-span-8">{info.direction}</span>
+                </li>
+                <li className="grid grid-cols-10 gap-2">
+                  <span className="col-span-2 text-right">가격</span>
+                  <span className="col-span-8">{info.hanPrc}</span>
+                </li>
+                <li className="grid grid-cols-10 gap-2">
+                  <span className="col-span-2 text-right">주거형태</span>
+                  <span className="col-span-8">{info.rletTpNm}</span>
+                </li>
+              </ul>
+            </div>
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Link to={url} className="w-full">
+              <Button className="w-full py-10">예방 결과 보러가기</Button>
+            </Link>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </li>
   );
 };
--- src/pages/ProtectList.jsx
+++ src/pages/ProtectList.jsx
@@ -28,8 +28,8 @@ export default function ProtectList() {
   }, []);
 
   return (
-    <main className="min-h-full flex flex-col items-center bg-slate-50">
-      <div className="min-h-full py-28 px-10 max-w-[800px] w-full bg-white border shadow-md">
+    <main className="min-h-full h-full flex flex-col items-center bg-slate-50">
+      <div className="min-h-full pt-28 px-10 max-w-[800px] w-full bg-white border shadow-md">
         <h1 className="text-center text-4xl font-semibold">
           전세 매물을 찾았습니다
         </h1>
@@ -37,59 +37,20 @@ export default function ProtectList() {
           총 <strong className="text-blue-400">{result.length}</strong> 건의
           매물이 검색되었습니다
         </h2>
-        <section className="pt-10 bg-rose-100">
+        <section className="mt-5 border-t p-5 max-h-[650px] overflow-y-auto">
           <ul className="space-y-5">
             {result.map((item, index) => {
-              return <JeonseCard2 info={item} key={index} />;
+              return (
+                <JeonseCard2
+                  info={item}
+                  key={index}
+                  url={`/protect/result/${item.atclNo}`}
+                />
+              );
             })}
           </ul>
         </section>
       </div>
     </main>
   );
-}
-
-{
-  /* <>
-<div className="min-h-[350px] flex flex-col justify-end mb-36 animate__animated animate__fadeIn">
-  <div className="text-center">
-    <div className="text-4xl">전세 매물 결과입니다</div>
-    <div>총 {protectList.length}건의 매물이 검색되었습니다</div>
-  </div>
-</div>
-{protectList.length === 0 ? (
-  <div className="flex flex-col items-center animate__animated animate__fadeInUp">
-    <div className="text-lg font-black text-gray-400">
-      검색 결과가 없다면?
-    </div>
-    <div
-      className="text-xl font-bold mt-3 bg-slate-400 text-white pl-7 pr-7 pt-3 pb-3 rounded-lg cursor-pointer"
-      onClick={() => {
-        navigate("/protect");
-      }}
-    >
-      다시 검색하러가기
-    </div>
-  </div>
-) : (
-  <>
-    <ul className="animate__animated animate__fadeIn">
-      {protectList.map((protect, index) => {
-        return (
-          <JeonseCard key={index} protect={protect} isProtect={true} />
-        );
-      })}
-    </ul>
-  </>
-)}
-
-<div
-  className="fixed top-[900px] left-[50px] cursor-pointer"
-  onClick={() => {
-    navigate("/protect");
-  }}
->
-  <IoIosArrowBack size="50" />
-</div>
-</> */
 }
