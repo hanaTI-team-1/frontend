e559e3243a35c6e9e6fa4ecb3bb321208352832f
gyueunnim <rnrmfrbdms@gmail.com>
2024-07-18 19:09:33 +0900

    오타 수정 및 model1 레포트 추가

--- src/components/card/InfoCard2.jsx
+++ src/components/card/InfoCard2.jsx
@@ -2,10 +2,10 @@ export const InfoCard2 = () => {
   return (
     <article className="relative p-5 pr-10 w-full h-40 flex justify-between bg-rose-200 rounded-lg shadow-lg">
       <hgroup>
-        <h4 className="text-sm font-bold opacity-70">상금채납자 조회</h4>
+        <h4 className="text-sm font-bold opacity-70">상습채납자 조회</h4>
 
         <h5 className="text-sm font-medium opacity-85">
-          혹시 나의 집주인이 상급채납자?!
+          혹시 나의 집주인이 상습채납자?!
         </h5>
         <a
           href="https://www.nts.go.kr/nts/cm/cntnts/cntntsView.do?mi=6685&cntntsId=8097"
